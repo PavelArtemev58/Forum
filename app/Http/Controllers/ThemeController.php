678fe0a0f4cabfcba327cbe41258095ae1601df2
PavelArtemev58 <megaundead58@gmail.com>
2023-02-03 13:40:00 +0300

    themes added

--- app/Http/Controllers/ThemeController.php
+++ app/Http/Controllers/ThemeController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Theme;
+use App\Models\Section;
 
 class ThemeController extends Controller
 {
     public function showThemes($section)
     {
+        $themes = Section::where('name', '=', $section)
+                ->first()
+                ->themes()
+                ->paginate(2);
         
+        return view ('main.themes', ['themes'=>$themes, 'section'=>$section]);
     }
 }
--- resources/views/main/themes.blade.php
+++ resources/views/main/themes.blade.php
@@ -1,11 +1,11 @@
 <x-guest-layout>
     <x-slot:header>
         <h1 class="font-semibold text-xl text-gray-800 leading-tight">
-            Forum
+            Themes of the {{ $section }}
         </h1>
     </x-slot>
     <!-- Login/Auth/Dashboard -->
-            @if (Route::has('login'))
+                @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
                         <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
@@ -19,17 +19,11 @@
                 </div>
             @endif
     
-   <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8"><h2>Sections</h2></div>
-            
-    @foreach ($sections as $section)
+    @foreach ($themes as $theme)
         <div class="mt-4">
-            <a href="/section/{{ $section->name }}">{{ $section->name }}</a>
+            <a href="/section/{{ $theme->name }}">{{ $theme->name }}</a>
         </div>
     @endforeach
     
-<!--    <div class="flex items-center justify-end mt-4">
-        <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-            Login
-        </a>
-    </div>-->
-</x-guest-layout>
+    
+</x-hyest-layout>
